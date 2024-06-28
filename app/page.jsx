c45e598771e28ba36c5243bac6e001b2588f9c0e
Abhishek singh <2237723.ece.cec@cgc.edu.in>
2024-06-28 14:57:37 +0530

    sidebar add

--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import WeatherCardLocation from './components/weatherCardLoaction/WeatherCardLocation'
 import WeaterCard from './components/weatherCardLoaction/WeatherCard/WeaterCard'
+import SideBar from './components/sidebar/SideBar'
 
 const page = () => {
   return (
     <div>
-      <div className='w-screen h-screen fixed top-0 left-0'>
+      <div className='w-screen h-screen fixed top-0 left-0 -z-50'>
         <img className='w-full h-full object-cover' src="/clouds.jpg" alt="cloudsbackground" />
       </div>
-      <WeatherCardLocation/>
-      <WeaterCard/>
+      {/* <WeaterCard/> */}
+      <SideBar/>
+      <WeatherCardLocation location={"delhi"} temprapure={"35 deg"}/>
+      
       
     </div>
   )
